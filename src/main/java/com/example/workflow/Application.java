73f83d33e2a5deedf1a5250780f07d89aa215420
Emmanuele Sanabra <sanabra@gmail.com>
2020-06-18 19:59:41 -0300

    exemplo simples https://www.youtube.com/watch?v=sHgf_EsQzfc

--- src/main/java/com/example/workflow/Application.java
+++ src/main/java/com/example/workflow/Application.java
@@ -1,4 +1,4 @@
-package com.sanabra.workflow;
+package com.example.workflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
