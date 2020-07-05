b7140d14f0ac5797936879b82b431c42c558193e
Emmanuele Sanabra <sanabra@gmail.com>
2020-07-05 15:44:39 -0300

    Finalizado Advanced Topic

---
+++ camunda-advanced-topics/src/test/01. Ir para aeroporto ID 14.sh
@@ -0,0 +1,7 @@
+curl --location --request POST 'http://localhost:8080/engine-rest/message' \
+--header 'Content-Type: application/json' \
+--header 'Host: localhost:8080' \
+--data-raw '{
+    "messageName": "MessageIrParaAeroporto",
+    "businessKey": 14
+}'
